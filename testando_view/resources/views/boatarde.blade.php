bd286bf3afae1861459c308bd9f7e0446d485d50
Aluno <aluno@linuxdev64>
2019-10-22 17:21:38 -0300

    teste view fim

---
+++ testando_view/resources/views/boatarde.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.app')
+
+@section('title', 'Boa tarde');
+    
+@section('content')
+  <h1>Boa tarde</h1>
+@endsection
+
--- testando_view/routes/web.php
+++ testando_view/routes/web.php
@@ -10,7 +10,24 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/info', function() {
+    phpinfo();
+});
+
+Route::middleware('verifica-hora')->get('/hello/{nome}/{idade?}', function(Request $r, $nome, $idade="-") {
+    echo($r->id);
+    return "Hello world, $nome!\nVc tem $idade anos";
+});
+
+Route::get('/boa-tarde/{nome?}', function($nome=""){
+    return view('boatarde', ['nome' => $nome]);
+});
+
+
+
